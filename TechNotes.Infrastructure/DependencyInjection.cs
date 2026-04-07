824db9b98d5f9f6bc63bca08a634aad80ddde8d6
Jhonny Remolina <asistenteinde@redsis.com>
2026-04-07 16:56:32 -0500

    Creacion dependencyinjection para conexion a bd.

---
+++ TechNotes.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TechNotes.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(
+            configuration.GetConnectionString("ConexionSql"),
+            b => b.MigrationsAssembly("TechNotes.Infrastructure")
+            ));
+        return services;
+    }
+
+}
